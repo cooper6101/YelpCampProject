b5868ef2d98a03534289db922a49cf6e025d67b4
Trey Cooper <cooper6101@gmail.com>
2019-09-04 11:47:48 +0000

    updated dburl

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://cooper6101:!Qsxdr5t@webdevbootcampyelpcamp-zc6px.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useCreateIndex: true}).then(() => {
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useCreateIndex: true}).then(() => {
 	console.log("Connected to DB!");
 }).catch(err => {
 	console.log("ERROR", err.message);
