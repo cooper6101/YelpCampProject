10ad71c39d5317b70a95e57a5c5e594bdb85c165
Trey Cooper <cooper6101@gmail.com>
2019-09-06 11:22:37 +0000

    added admin priveledges

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,6 @@
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
+var User = require("../models/user");
 
 //MIDDLEWARE
 var middlewareObj = {};
@@ -12,7 +13,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
 				res.redirect("back");
 			} else {
 				//does user own campground
-				if(foundCampground.author.id.equals(req.user._id)) {
+				if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
 					next();						
 				} else {
 					req.flash("error", "You don't have permission to do that!");
@@ -35,7 +36,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
 				res.redirect("back");
 			} else {
 				//does user own comment
-				if(foundComment.author.id.equals(req.user._id)) {
+				if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
 					next();						
 				} else {
 					req.flash("error", "You don't have permission to do that!");
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -35,6 +35,7 @@ router.get("/new", middleware.isLoggedIn, function(req, res){
 router.post("/", middleware.isLoggedIn, function(req, res){
   // get data from form and add to campgrounds array
   var name = req.body.name;
+  var price = req.body.price;	
   var image = req.body.image;
   var desc = req.body.description;
   var author = {
